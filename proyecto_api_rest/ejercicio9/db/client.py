e10598ba53beeb5ab358ca222c0516b6dd125796
jl-manzano <jl.manzano@iesnervion.es>
2025-12-17 09:27:44 +0100

    commit

--- proyecto_api_rest/ejercicio9/db/client.py
+++ proyecto_api_rest/ejercicio9/db/client.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
 
 # Base de datos en local
-db_client = MongoClient()
+db_client = MongoClient("mongodb+srv://jlmanzano_db_user:dU3YsgDXBxs85MOs@cluster0.djy98m0.mongodb.net/?appName=Cluster0")
