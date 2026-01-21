35e6064d80d875e640e5d48bfbe2eccbf7565f59
jl-manzano <jl.manzano@iesnervion.es>
2026-01-21 12:52:53 +0100

    Update ejer1.py

--- prog_procesos/ejer1.py
+++ prog_procesos/ejer1.py
@@ -5,17 +5,14 @@ def sumar_numeros(limite):
     print(f"Suma de 1 hasta {limite}: {suma}")
 
 if __name__ == '__main__':
-    # Crear procesos
     p1 = Process(target=sumar_numeros, args=(10,))
     p2 = Process(target=sumar_numeros, args=(20,))
     p3 = Process(target=sumar_numeros, args=(50,))
     
-    # Iniciar procesos
     p1.start()
     p2.start()
     p3.start()
     
-    # Esperar a que terminen
     p1.join()
     p2.join()
     p3.join()
